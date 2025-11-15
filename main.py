8b377bce6ccb06b9213bcf8170eff90d284231b7
walesoje1 <31517688+walesoje1@users.noreply.github.com>
2025-11-15 13:59:10 +0100

    data curation program

---
+++ main.py
@@ -0,0 +1,43 @@
+import csv
+import pandas as pd
+from pathlib import Path
+
+
+def csv_content_reader(file_path, search_criteria):
+	'''Reads the content of csv and filter based on search criteria on a column name'''
+	try:
+		df = pd.read_csv(file_path)
+		print(df)
+		df = df[df["product"] == search_criteria]
+		df["price"] = df["price"].str.replace("$", "", regex=False).astype(float)
+		df["sales"] = df["price"] * df["quantity"]
+		df_new_list = df[["sales", "date", "region"]]
+	except AttributeError:
+		print(f"File {file_path.name} does not have one of the attributes processed. Consider manual processing...")
+	except Exception as e:
+		print(f"An error occurred: {e}")
+	else:
+		return df
+
+
+def load_csv_content(search_criteria):
+	path = Path()
+	print(path)
+	data_path = path / "data"
+
+	df_list = []
+
+	for file_path in data_path.rglob(f"*{'.csv'}"):
+		print(f"Data Path: {data_path}\nFile path is {file_path}")
+		df = csv_content_reader(file_path, search_criteria)
+		df_list.append(df)
+
+	merge_df = pd.concat(df_list, ignore_index = True)
+	df_list.clear()
+
+	merge_df.to_csv(data_path / 'sales_data.csv')
+
+
+if __name__ == "__main__":
+	search_criteria = "pink morsel"
+	load_csv_content(search_criteria)
