9aed6bdc724b724c415694ab833bad6719187a9d
walesoje1 <31517688+walesoje1@users.noreply.github.com>
2025-11-15 14:21:05 +0100

    data curation program

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def csv_content_reader(file_path, search_criteria):
 	'''Reads the content of csv and filter based on search criteria on a column name'''
 	try:
 		df = pd.read_csv(file_path)
-		print(df)
 		df = df[df["product"] == search_criteria]
 		df["price"] = df["price"].str.replace("$", "", regex=False).astype(float)
 		df["sales"] = df["price"] * df["quantity"]
@@ -17,23 +16,22 @@ def csv_content_reader(file_path, search_criteria):
 	except Exception as e:
 		print(f"An error occurred: {e}")
 	else:
-		return df
+		return df[["date", "sales", "region"]]
 
 
 def load_csv_content(search_criteria):
 	path = Path()
-	print(path)
 	data_path = path / "data"
 
 	df_list = []
 
 	for file_path in data_path.rglob(f"*{'.csv'}"):
-		print(f"Data Path: {data_path}\nFile path is {file_path}")
 		df = csv_content_reader(file_path, search_criteria)
 		df_list.append(df)
 
 	merge_df = pd.concat(df_list, ignore_index = True)
 	df_list.clear()
+	print(merge_df)
 
 	merge_df.to_csv(data_path / 'sales_data.csv')
 
